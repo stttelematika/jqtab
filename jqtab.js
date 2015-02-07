aac69a7073d4a2d176555311a99080ad721b1cb3
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2015-02-07 17:43:50 +0800

    komit pertama

---
+++ jqtab.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	// sembunyikan semua div isi
+	$('.isi div').hide();
+ 
+	// tampilkan hanya div isi pada anak pertama
+	$('.isi div:first').show();
+ 
+	// saat tombol diklik	
+	$('.sc-btn a').click(function(){
+ 
+		// isi attribut class sema tombol dengan nilai 'non-aktif'
+		$('.sc-btn a').attr('class','non-aktif');
+ 
+		// isi attribut class tombol yang sedang aktif dengan nilai 'aktif'
+		$(this).attr('class','aktif');
+ 
+		// buat variabel isi, untuk menampung identitas div isi
+		var isi = '\'.isi div[name="' + this.name + '"]\'';
+ 
+		// sembunyikan semua div isi
+		$('.isi div').hide();
+ 
+		// tampilkan div isi dengan identitas yang telah didapatkan
+		$(isi).slideDown('fast');
+ 
+	});
+ 
+});
